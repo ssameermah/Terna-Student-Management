f3e3a98c63f8d7c63ca03861cc552e30bd4ee0b7
Sameer Mahajan <sam241099@gmail.com>
2020-09-26 11:53:46 +0530

    add subject

--- student_management/student_app/HodViews.py
+++ student_management/student_app/HodViews.py
@@ -85,3 +85,27 @@ def add_student_save(request):
         except:
             messages.error(request,"Failed to Add Student")
             return HttpResponseRedirect("/add_student")
+
+def add_subject(request):
+    courses=Courses.objects.all()
+    staffs=CustomUser.objects.filter(user_type=2)
+    return render(request,"hod_template/add_subject_template.html",{"staffs":staffs,"courses":courses})
+
+def add_subject_save(request):
+    if request.method!="POST":
+        return HttpResponse("<h2>Method Not Allowed</h2>")
+    else:
+        subject_name=request.POST.get("subject_name")
+        course_id=request.POST.get("course")
+        course=Courses.objects.get(id=course_id)
+        staff_id=request.POST.get("staff")
+        staff=CustomUser.objects.get(id=staff_id)
+
+        try:
+            subject=Subjects(subject_name=subject_name,course_id=course,staff_id=staff)
+            subject.save()
+            messages.success(request,"Successfully Added Subject")
+            return HttpResponseRedirect("/add_subject")
+        except:
+            messages.error(request,"Failed to Add Subject")
+            return HttpResponseRedirect("/add_subject")
